11b4ce9eec8709ca2285bdd3c5fecf93d6cd92b7
Matt Lehman <matt.lehman@gmail.com>
2011-03-08 17:19:43 -0500

    Remove down casing of http headers

--- src/clj_http/core.clj
+++ src/clj_http/core.clj
@@ -8,7 +8,7 @@
   (:import (org.apache.http.impl.client DefaultHttpClient)))
 
 (defn- parse-headers [#^HttpResponse http-resp]
-  (into {} (map (fn [#^Header h] [(.toLowerCase (.getName h)) (.getValue h)])
+  (into {} (map (fn [#^Header h] [(.getName h) (.getValue h)])
                 (iterator-seq (.headerIterator http-resp)))))
 
 (defn request
